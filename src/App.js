f50748c8c342a436d056a0d45666086a382ae433
GarrettPhD <111952712+GarrettPhD@users.noreply.github.com>
2023-04-19 20:12:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import TicTacToeNav from './Layout/Nav/NavBar';
+import GameBoard from './GameBoard';
 import UserComponent from './Users/UserComponent';
 
 function App() {
   return (
     <div className="App">
       <TicTacToeNav />
-      <UserComponent />
+      <div className="container mt-5">
+        <GameBoard />
+        <UserComponent />
+      </div>
     </div>
   );
 }
